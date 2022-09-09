f71941f06be1f06f727a8c1ff0e745f7afdb4ba8
unknown <429193-13>
2022-09-09 14:47:09 +0300

    first commit

---
+++ game_2048/2048/Help_window.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace _2048
+{
+    public partial class Help_window : Form
+    {
+        Form1 form;
+
+        public Help_window()
+        {
+            InitializeComponent();
+        }
+
+        private void Help_window_Load(object sender, EventArgs e)
+        {
+            form = (Form1)this.Owner; //задание владельца формы
+        }
+
+        private void Help_window_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            form.helpWindow_closed = true; //передача в основную форму информации, что справка закрыта
+        }
+    }
+}
---
+++ game_2048/Model/Map_of_tile.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Model
+{
+    class Map_of_tile
+    {
+        int[,] map_tile; //двумерный массив для хранения чисел на плитах
+
+        /// <summary>
+        /// Конструктор без параметров, в котором выделяется память под масив, хранящий значения на плитках
+        /// </summary>
+        public Map_of_tile()
+        {
+            map_tile = new int[4, 4];
+        }
+
+        /// <summary>
+        /// Функция получения числа на плитке
+        /// </summary>
+        /// <param name="x">Номер строки, в которой находится плитка</param>
+        /// <param name="y">Номер столбца, в котором находится плитка</param>
+        /// <returns></returns>
+        public int Get(int x, int y)
+        {
+            if(x>=0&&x<4 && y>=0 && y < 4)
+                return map_tile[x, y];
+            return -1; //возврат -1, если запрошено значение с плитки вне поля
+        }
+
+        /// <summary>
+        /// Метод, устанавливающий число на плитке
+        /// </summary>
+        /// <param name="x">Номер строки, в которой находится плитка</param>
+        /// <param name="y">Номер столбца, в котором находится плитка</param>
+        /// <param name="num">Устанавливаемое число</param>
+        public void Set(int x, int y, int num)
+        {
+            if (x >= 0 && x < 4 && y >= 0 && y < 4)
+                map_tile[x, y] = num;
+        }
+    }
+}
